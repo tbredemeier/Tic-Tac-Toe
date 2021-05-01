0713494a5611d3cba4817d40ed98fcd27242dfb8
Tom Bredemeier <tbredemeier@barrington220.org>
2021-05-01 00:40:05 -0500

    Added color transition

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var moves = Array(repeating: "", count: 9)
+    @State var xTurn = true
     var body: some View {
         NavigationView {
             VStack {
@@ -16,12 +18,26 @@ struct ContentView: View {
                             ForEach(0..<9, id: \.self) { index in
                                 ZStack {
                                     Color.blue
-                                    Text("X")
+                                    Color.white
+                                        .opacity(moves[index] == "" ? 1 : 0)
+                                    Text(moves[index])
                                         .font(.system(size: 90))
                                         .fontWeight(.heavy)
                                 }
                                 .frame(width: 120, height: 120, alignment: .center)
                                 .cornerRadius(30)
+                                .onTapGesture {
+                                    withAnimation(Animation.default) {
+                                        if moves[index] == "" {
+                                            moves[index] = xTurn ? "X" : "O"
+                                            xTurn.toggle()
+                                        }
+                                    }
+                                }
+                                .rotation3DEffect(
+                                    .init(degrees: moves[index] != "" ? 180 : 0),
+                                    axis: (x: 0.0, y: 1.0, z: 0.0)
+                                )
                             }
                           })
             }
