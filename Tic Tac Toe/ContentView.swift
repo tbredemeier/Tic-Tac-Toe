8c557bef742c3ec7c2e86b698173cc19bf668330
Tom Bredemeier <tbredemeier@barrington220.org>
2021-05-03 13:27:24 -0500

    Determined if game is a draw

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -68,6 +68,10 @@ struct ContentView: View {
         checkLine(a: 2, b: 5, c: 8)
         checkLine(a: 0, b: 4, c: 8)
         checkLine(a: 6, b: 4, c: 2)
+        if !moves.contains("") {
+            winMessage = "Cat's Game"
+            gameOver = true
+        }
     }
     
     private func checkLine(a: Int, b: Int, c: Int) {
