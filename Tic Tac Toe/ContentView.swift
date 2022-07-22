6890ece9c459c7bc1d0f296fb32a239e60c95e28
Tom Bredemeier <tbredemeier@barrington220.org>
2022-07-22 14:33:06 -0500

    Added text to cell tiles

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -16,9 +16,14 @@ struct ContentView: View {
                 .padding()
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
                 ForEach(0..<9) { index in
-                    Color.blue
-                        .frame(width: 120, height: 120, alignment: .center)
-                        .cornerRadius(30)
+                    ZStack {
+                        Color.blue
+                        Text("X")
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
                 }
             }
         }
