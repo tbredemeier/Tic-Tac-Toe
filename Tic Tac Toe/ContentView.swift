0ebd3f590bf7f1b56e6e3cd9e08413213caafbd2
Tom Bredemeier <tbredemeier@barrington220.org>
2022-07-22 14:30:11 -0500

    Created cell tiles

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -15,15 +15,11 @@ struct ContentView: View {
                 .fontWeight(.bold)
                 .padding()
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
+                ForEach(0..<9) { index in
+                    Color.blue
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .cornerRadius(30)
+                }
             }
         }
         .preferredColorScheme(.dark)
