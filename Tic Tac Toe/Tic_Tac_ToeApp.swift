6de0823c019847fa093d3e43ff7fce671df3fc5e
Tom Bredemeier <tbredemeier@barrington220.org>
2022-07-22 14:11:02 -0500

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Tom Bredemeier on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
