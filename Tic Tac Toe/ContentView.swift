69415a2df34639e3f8ab0a8e505c8038aaa6272a
Tom Bredemeier <tbredemeier@barrington220.org>
2024-11-18 10:07:25 -0600

    Added rotation animation

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
                 ForEach(0..<9) {index in
                     ZStack {
                         Color.blue
+                        Color.white.opacity(moves[index] == "" ? 1: 0)
                         Text(moves[index])
                             .font(.system(size: 90))
                             .fontWeight(.heavy)
@@ -31,6 +32,7 @@ struct ContentView: View {
                             }
                         }
                     }
+                    .rotation3DEffect(.degrees(moves[index] == "" ? 180 : 0), axis: (0, 1, 0))
                 }
             }
         }
