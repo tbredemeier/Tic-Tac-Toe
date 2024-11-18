04d17b3c25176bf5fb65c6bfb5ab9254ddcc852c
Tom Bredemeier <tbredemeier@barrington220.org>
2024-11-18 09:50:40 -0600

    Added Xs and Os

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
     var body: some View {
         VStack {
             Text("Tic Tac Toe").font(.title).bold()
@@ -23,7 +24,12 @@ struct ContentView: View {
                     .frame(width: 120, height: 120, alignment: .center)
                     .cornerRadius(30)
                     .onTapGesture {
-                        moves[index] = "X"
+                        withAnimation {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "O"
+                                xTurn.toggle()
+                            }
+                        }
                     }
                 }
             }
