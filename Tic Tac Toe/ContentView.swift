1d1fbaf5c3c6255b2e5d421b5e9ba0cb74c5bec0
Tom Bredemeier <tbredemeier@barrington220.org>
2025-11-10 10:52:00 -0600

    Added 3 by 3 grid

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -13,15 +13,11 @@ struct ContentView: View {
             Text("Tic Tac Toe")
                 .font(.title).bold()
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3)) {
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
+                ForEach(0..<9) {index in
+                    Color.blue
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .cornerRadius(30)
+                }
             }
         }
         .preferredColorScheme(.dark)
