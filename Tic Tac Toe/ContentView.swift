ee7564d52904175ee1f8e1e3738630211db6c54f
Tom Bredemeier <tbredemeier@barrington220.org>
2025-11-11 10:55:18 -0600

    Added game reset function

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -41,7 +41,12 @@ struct ContentView: View {
         }
         .preferredColorScheme(.dark)
         .alert(isPresented: $gameOver) {
-            Alert(title: Text(winMessage))
+            Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play again"), action: {
+                withAnimation {
+                    moves = Array(repeating: "", count: 9)
+                    gameOver = false
+                }
+            }))
         }
         .onChange(of: moves) { oldValue, newValue in
             checkForWinner()
